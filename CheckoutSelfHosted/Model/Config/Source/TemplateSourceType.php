d2affada5c48ac43c03b3d8c8bd56d9c37bafc4f
MykolaMalovanets <mykola.malovanets@gmail.com>
2023-06-02 16:59:36 +0300

    Magento 2 Self-hosted checkout.

---
+++ CheckoutSelfHosted/Model/Config/Source/TemplateSourceType.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace Bold\CheckoutSelfHosted\Model\Config\Source;
+
+use Bold\Checkout\Model\ConfigInterface;
+use Magento\Framework\Data\OptionSourceInterface;
+
+/**
+ * "Bold Checkout Template Type" system configuration source class.
+ */
+class TemplateSourceType implements OptionSourceInterface
+{
+    public const VALUE_TYPE_ONE_PAGE= 'one_page';
+    public const VALUE_TYPE_THREE_PAGE= 'three_page';
+
+    /**
+     * @inheritDoc
+     */
+    public function toOptionArray(): array
+    {
+        return [
+            ['value' => self::VALUE_TYPE_THREE_PAGE, 'label' => __('Three Page (Default)')],
+            ['value' => self::VALUE_TYPE_ONE_PAGE, 'label' => __('One Page')],
+        ];
+    }
+}
---
+++ CheckoutSelfHosted/registration.php
@@ -0,0 +1,5 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Bold_CheckoutSelfHosted', __DIR__);
