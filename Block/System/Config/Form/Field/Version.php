37e5e19dc87b2cf8b211cd5b8521c21abc31cfe7
Pavel Bystritsky <p.bystritskyi@gmail.com>
2023-09-27 10:52:43 +0200

    Q1-539: M2 - Show module version in admin.

--- Block/System/Config/Form/Field/Version.php
+++ Block/System/Config/Form/Field/Version.php
@@ -8,7 +8,6 @@ use Bold\Checkout\Model\ModuleVersionProvider;
 use Magento\Backend\Block\Template\Context;
 use Magento\Config\Block\System\Config\Form\Field;
 use Magento\Framework\Data\Form\Element\AbstractElement;
-use Magento\Framework\View\Helper\SecureHtmlRenderer;
 
 /**
  * Bold Integration version field.
@@ -24,15 +23,13 @@ class Version extends Field
      * @param Context $context
      * @param ModuleVersionProvider $moduleVersionProvider
      * @param array $data
-     * @param SecureHtmlRenderer|null $secureRenderer
      */
     public function __construct(
         Context $context,
         ModuleVersionProvider $moduleVersionProvider,
-        array $data = [],
-        ?SecureHtmlRenderer $secureRenderer = null
+        array $data = []
     ) {
-        parent::__construct($context, $data, $secureRenderer);
+        parent::__construct($context, $data);
         $this->moduleVersionProvider = $moduleVersionProvider;
     }
 
