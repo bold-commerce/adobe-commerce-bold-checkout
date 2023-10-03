94265f085e06e3fd5abd7ce50098c0337c55b945
MykolaMalovanets <mykola.malovanets@gmail.com>
2023-10-03 15:55:50 +0300

    Improve self-hosted checkout.

--- Observer/Order/ProcessOrderObserver.php
+++ Observer/Order/ProcessOrderObserver.php
@@ -3,14 +3,11 @@ declare(strict_types=1);
 
 namespace Bold\Checkout\Observer\Order;
 
-use Bold\Checkout\Api\Data\Http\Client\ResultInterface;
 use Bold\Checkout\Api\Http\ClientInterface;
-use Bold\Checkout\Model\Order\Address\Converter;
 use Magento\Checkout\Model\Session;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
 use Magento\Framework\Exception\LocalizedException;
-use Magento\Sales\Api\Data\OrderInterface;
 
 /**
  * Place order on Bold side observer.
@@ -28,23 +25,15 @@ class ProcessOrderObserver implements ObserverInterface
     private $client;
 
     /**
-     * @var Converter
-     */
-    private $addressConverter;
-
-    /**
      * @param Session $checkoutSession
      * @param ClientInterface $client
-     * @param Converter $addressConverter
      */
     public function __construct(
         Session $checkoutSession,
-        ClientInterface $client,
-        Converter $addressConverter
+        ClientInterface $client
     ) {
         $this->checkoutSession = $checkoutSession;
         $this->client = $client;
-        $this->addressConverter = $addressConverter;
     }
 
     /**
@@ -65,45 +54,11 @@ class ProcessOrderObserver implements ObserverInterface
             return;
         }
         $websiteId = (int)$order->getStore()->getWebsiteId();
-        $result = $this->syncAddress($order, $websiteId);
-        if ($result->getErrors()) {
+        $refresh = $this->client->get($websiteId, 'refresh');
+        $taxes = $this->client->post($websiteId, 'taxes', []);
+        $boldOrder = $this->client->post($websiteId, 'process_order', []);
+        if ($boldOrder->getErrors() || $taxes->getErrors() || $refresh->getErrors()) {
             throw new LocalizedException(__('Something went wrong. Please try to place the order again.'));
         }
-        $subRequests = [
-            'sub_requests' => [
-                [
-                    'method' => 'POST',
-                    'endpoint' => '/taxes',
-                    'payload' => new \stdClass(),
-                ],
-                [
-                    'method' => 'POST',
-                    'endpoint' => '/process_order',
-                    'payload' => new \stdClass(),
-                ],
-            ],
-        ];
-        $boldOrder = $this->client->post($websiteId, 'batch', $subRequests);
-        if ($boldOrder->getErrors()) {
-            throw new LocalizedException(__('Something went wrong. Please try to place the order again.'));
-        }
-    }
-
-    /**
-     * Synchronize shipping address with Bold in case order is not virtual. Update billing address otherwise.
-     *
-     * @param OrderInterface $order
-     * @param int $websiteId
-     * @return ResultInterface
-     * @throws \Exception
-     */
-    private function syncAddress(OrderInterface $order, int $websiteId): ResultInterface
-    {
-        if (!$order->getIsVirtual()) {
-            $addressPayload = $this->addressConverter->convert($order->getShippingAddress());
-            return $this->client->post($websiteId, 'addresses/shipping', $addressPayload);
-        }
-        $addressPayload = $this->addressConverter->convert($order->getBillingAddress());
-        return $this->client->post($websiteId, 'addresses/billing', $addressPayload);
     }
 }
