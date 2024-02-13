3c16d8d6860fc94fe0e4507621e01c3f25eda81a
nicole.norman <nicole.norman@boldcommerce.com>
2024-02-13 12:29:45 -0600

    Add hmac validation for connector endpoint

--- Block/System/Config/Form/Field/OnboardBanner.php
+++ Block/System/Config/Form/Field/OnboardBanner.php
@@ -2,26 +2,22 @@
 
 namespace Bold\Checkout\Block\System\Config\Form\Field;
 
-use Bold\Checkout\Model\ConfigInterface;
 use Bold\Checkout\Model\Http\Client\RequestsLogger;
+use Bold\Checkout\Model\Http\PlatformClient;
 use Magento\Backend\Block\Template\Context;
 use Magento\Config\Block\System\Config\Form\Field;
 use Magento\Framework\Data\Form\Element\AbstractElement;
-use Magento\Framework\HTTP\ClientInterface;
 use Magento\Store\Model\StoreManagerInterface;
 
 class OnboardBanner extends Field
 {
-    private const ONBOARD_DATA_PATH = '/onboard_banner_data';
+    private const ONBOARD_DATA_PATH = '/{{shopId}}/onboard_banner_data';
 
     /** @var StoreManagerInterface */
     private $storeManager;
 
-    /** @var ConfigInterface */
-    private $config;
-
-    /** @var ClientInterface  */
-    private $client;
+    /** @var PlatformClient */
+    private $platformClient;
 
     /** @var RequestsLogger */
     private $logger;
@@ -32,14 +28,12 @@ class OnboardBanner extends Field
     public function __construct(
         Context $context,
         StoreManagerInterface $storeManager,
-        ConfigInterface $config,
-        ClientInterface $client,
+        PlatformClient $platformClient,
         RequestsLogger $logger,
     ) {
         parent::__construct($context, []);
         $this->storeManager = $storeManager;
-        $this->config = $config;
-        $this->client = $client;
+        $this->platformClient = $platformClient;
         $this->logger = $logger;
     }
 
@@ -61,17 +55,13 @@ class OnboardBanner extends Field
     public function getBannerData()
     {
         $websiteId = $this->storeManager->getWebsite()->getId();
-        $connectorApiUrl = $this->config->getPlatformConnectorUrl($websiteId);
-        $connectorBaseUrl = parse_url($connectorApiUrl, PHP_URL_SCHEME) . '://' . parse_url($connectorApiUrl, PHP_URL_HOST);
-        $requestUrl = $connectorBaseUrl . self::ONBOARD_DATA_PATH;
+        $bannerData = $this->platformClient->get($websiteId, self::ONBOARD_DATA_PATH);
 
-        $this->client->get($requestUrl);
-
-        if ($this->client->getStatus() !== 200) {
-            $this->logger->logRequest($websiteId, $requestUrl, 'GET');
+        if ($bannerData->getStatus() !== 200) {
+            $this->logger->logRequest($websiteId, self::ONBOARD_DATA_PATH, 'GET');
             return null;
         }
 
-        return json_decode($this->client->getBody());
+        return $bannerData->getBody();
     }
 }
