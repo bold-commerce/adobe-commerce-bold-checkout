7cb9c1a1d5b2d9cbbd07d768c8de4e9011cc0c95
nicole.norman <nicole.norman@boldcommerce.com>
2024-06-10 11:01:17 -0500

    Split resume and simple resume into two methods

--- Model/Order/ResumeOrder.php
+++ Model/Order/ResumeOrder.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 namespace Bold\Checkout\Model\Order;
 
 use Bold\Checkout\Api\Http\ClientInterface;
-use Bold\CheckoutMeta\Model\Config;
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Quote\Api\Data\CartInterface;
@@ -23,19 +22,10 @@ class ResumeOrder
     private $client;
 
     /**
-     * @var Config
-     */
-    private $checkoutMetaConfig;
-
-    /**
      * @param ClientInterface $client
      */
-    public function __construct(
-        ClientInterface $client,
-        Config $checkoutMetaConfig
-    ) {
+    public function __construct(ClientInterface $client) {
         $this->client = $client;
-        $this->checkoutMetaConfig = $checkoutMetaConfig;
     }
 
     /**
@@ -54,12 +44,30 @@ class ResumeOrder
             'public_order_id' => $publicOrderId
         ];
 
-        $useFastCheckout = $this->checkoutMetaConfig->getUseFastCheckout($websiteId);
-        $simpleResumeUrl = sprintf(self::RESUME_SIMPLE_URL, $publicOrderId);
+        $orderData = $this->client->post($websiteId, self::RESUME_URL, $body)->getBody();
 
-        $orderData = $useFastCheckout
-            ? $this->client->post($websiteId, $simpleResumeUrl, [])->getBody()
-            : $this->client->post($websiteId, self::RESUME_URL, $body)->getBody();
+        $publicOrderId = $orderData['data']['public_order_id'] ?? null;
+        if (!$publicOrderId) {
+            throw new LocalizedException(__('Cannot resume order'));
+        }
+
+        return $orderData;
+    }
+
+    /**
+     * Resume a simple order on Bold's side to get a refreshed JWT.
+     *
+     * @param CartInterface $quote
+     * @param string $publicOrderId
+     * @return array
+     * @throws LocalizedException
+     * @throws NoSuchEntityException
+     */
+    public function resumeSimpleOrder(CartInterface $quote, string $publicOrderId): array
+    {
+        $websiteId = (int)$quote->getStore()->getWebsiteId();
+        $simpleResumeUrl = sprintf(self::RESUME_SIMPLE_URL, $publicOrderId);
+        $orderData = $this->client->post($websiteId, $simpleResumeUrl, [])->getBody();
 
         $publicOrderId = $orderData['data']['public_order_id'] ?? null;
         if (!$publicOrderId) {
