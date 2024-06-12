6ede48fed67665150a599509a4f18f8096d3a0e7
Joseph Leedy <joseph.leedy@boldcommerce.com>
2024-06-12 13:53:17 -0500

    Configure Rector to downgrade code for PHP 7.2

---
+++ rector.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\DowngradeLevelSetList;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/app',
+    ])
+    ->withSets([
+        DowngradeLevelSetList::DOWN_TO_PHP_72
+    ]);
