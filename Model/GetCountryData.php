aabe600b907473e677dc6fe6ddfd3d665c03a95a
nicole.norman <nicole.norman@boldcommerce.com>
2024-08-27 12:09:46 -0500

    Add Custom Country Data Endpoint

---
+++ Model/GetCountryData.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Bold\Checkout\Model;
+
+use Bold\Checkout\Api\GetCountryDataInterface;
+use Magento\Directory\Api\CountryInformationAcquirerInterface;
+use Magento\Directory\Api\Data\CountryInformationInterface;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Model\ScopeInterface;
+use Magento\Store\Model\StoreManagerInterface;
+
+class GetCountryData implements GetCountryDataInterface
+{
+    /**
+     * @var StoreManagerInterface
+     */
+    private $storeManager;
+
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
+     * @var CountryInformationAcquirerInterface
+     */
+    private $countryInformationAcquirer;
+
+    public function __construct(
+        StoreManagerInterface $storeManager,
+        ScopeConfigInterface $scopeConfig,
+        CountryInformationAcquirerInterface $countryInformationAcquirer
+    ) {
+        $this->storeManager = $storeManager;
+        $this->scopeConfig = $scopeConfig;
+        $this->countryInformationAcquirer = $countryInformationAcquirer;
+    }
+
+    public function getData(string $countryId): CountryInformationInterface
+    {
+        $storeId = $this->storeManager->getStore()->getId();
+        $countriesRequiringStates = $this->scopeConfig->getValue('general/region/state_required', ScopeInterface::SCOPE_STORE, $storeId);
+        $statesOptional = !in_array($countryId, explode(',', $countriesRequiringStates));
+        $countryData = $this->countryInformationAcquirer->getCountryInfo($countryId);
+
+        if ($statesOptional) {
+            $countryData->setAvailableRegions([]);
+        }
+
+        return $countryData;
+    }
+}
